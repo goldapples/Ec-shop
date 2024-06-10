7e6f23d6559442238a0cdffaf3ca60b6e8c5f841
Seed <devcomposer0213@gmail.com>
2024-06-10 23:45:03 +0900

    update: product_v3

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -5,7 +5,6 @@ const requireAuth = passport.authenticate("jwt", { session: false });
 const ProductsCtr = require("../controllers/productsCtr");
 
 router.post("/products/create", requireAuth, ProductsCtr.create);
-router.post("/products/getbyguest", ProductsCtr.getAllByGuest);
 router.get("/products/get", requireAuth, ProductsCtr.getAll);
 router.get("/products/images", requireAuth, ProductsCtr.images);
 router.delete("/products/delete/:id", requireAuth, ProductsCtr.delete);
