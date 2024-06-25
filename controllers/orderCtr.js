a1ffbccd8b734634c368dc51f2ff80f913b8dccf
Panda <panda@gmail.com>
2024-06-25 14:31:35 +0900

    fix:order display according to role

--- controllers/orderCtr.js
+++ controllers/orderCtr.js
@@ -1,12 +1,21 @@
+const { filter } = require("async");
 const Order = require("../Models/orderModel");
 const Sale = require("../Models/productSalesModel");
+const Role = require("../Models/roleModel")
 
 exports.getAll = async (req, res) => {
     try {
         const orders = await Order.find().populate(
             { path: "user products.product store" }
-        )
-        res.status(200).json({ type: "success", message: "Get all orders successfully!", orders: orders })
+        ).sort({ createdAt: -1 })
+        const user = await Role.findById(req.user.role)
+        if (user.title === "admin") {
+            res.status(200).json({ type: "success", message: "Get all orders successfully!", orders: orders })
+        } else if (req.user.role !== "") {
+            const filterOrders = orders.filter((item) => String(item.store._id) === String(req.user.store))
+            console.log(filterOrders)
+            res.status(200).json({ type: "success", message: "Get all orders successfully!", orders: filterOrders })
+        }
     } catch (err) {
         res.status(400).json({ type: "error", message: err.message });
     }
@@ -26,11 +35,11 @@ exports.setState = async (req, res) => {
                     store: order.store,
                     person: order.user,
                     date: order.createdAt,
-                    category:item.product.category,
-                    product:item.product._id,
-                    price:item.product.price,
-                    sales_cnt:item.quantity,
-                    delete:false
+                    category: item.product.category,
+                    product: item.product._id,
+                    price: item.product.price,
+                    sales_cnt: item.quantity,
+                    delete: false
                 })
                 newSale.save()
             })
